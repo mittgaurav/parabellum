155be0a8cce895fd2c85f44caf6811465f33c08c
mittgaurav <gm_5315@yahoo.com>
2019-02-12 12:55:56 +0530

    Create word_reverse.cpp

---
+++ word_reverse.cpp
@@ -0,0 +1,30 @@
+#include <iostream>
+#include <string.h>
+
+void reverse(char* string, size_t i, size_t j) {
+    while( i < j ) {
+        char temp = string[i];
+        string[i] = string[j];
+        string[j] = temp;
+        i++; j--;
+    }
+}
+int main() {
+    char string[] = "  i l  ike to   code";
+    std::cout << string << std::endl;
+  
+    reverse(string, 0, strlen(string) - 1);
+    std::cout << string << std::endl;
+
+    size_t prev = 0;  // start of word
+    for( size_t i = 0; i <= strlen(string); i++) {
+        if(string[i] == ' ' || string[i] == '\0') {
+            if(i > prev) {  // optimization for spaces
+                reverse(string, prev, i-1);
+            }
+            prev = i + 1;  // start of next word
+        }
+    }
+
+    std::cout << string << std::endl;
+}
